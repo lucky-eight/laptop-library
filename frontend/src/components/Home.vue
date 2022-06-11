37fbd65575cae5599c12d4c7d69b0a0f8c509a1f
Vanessa <vanessa@limesharp.net>
2022-06-11 18:06:44 +0100

    initialise form and result components

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ApplicationForm v-if="!submitted" />
+    <Result v-else :is-success="result.isSuccess" />
+  </div>
+</template>
+
+<script>
+import ApplicationForm from "application-form.vue";
+import Result from "result.vue";
+export default {
+  name: "Home",
+
+  data() {
+    return {
+      result: {
+        isSuccess: null,
+        deliveryDate: "",
+      },
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
