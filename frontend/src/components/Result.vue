37c67922e440e9c0723d4cc7b7a6ee600515d825
Vanessa <vanessa@limesharp.net>
2022-06-11 18:06:44 +0100

    initialise form and result components

---
+++ frontend/src/components/Result.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="result-wrapper">
+    <div v-if="isSuccess && deliveryDate" class="success-wrapper">
+      <p>We've found a match!</p>
+      <p>You can expect to receive a laptop by {{ deliveryDate }}</p>
+    </div>
+    <div v-else class="unsuccess-wrapper">
+      <p>Unfortunately there are no laptops currently available</p>
+      <p>We will be in touch when we've found a match</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Result",
+  props: {
+    isSuccess: {
+      type: Boolean,
+      default: false,
+    },
+    deliveryDate: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped></style>
