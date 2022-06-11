e66b9e8554301077b1845ab652a7856b0cedd3a6
technologeve <eve.nicole@icloud.com>
2022-06-11 18:33:08 +0100

    Update read_laptops_csv

--- server/read_laptops_csv.py
+++ server/read_laptops_csv.py
@@ -2,8 +2,11 @@
 import pandas as pd
 
 def read_laptops_csv(laptops_csv):
-    db = pd.read_csv(laptops_csv, index_col="ID")
-    print(db)
-    return db
+    """ Reads a CSV and converts it to a list of tuples
+    to be fed into a database through sqlite3."""
 
-read_laptops_csv("server/laptops.csv")
+    data_frame = pd.read_csv(laptops_csv)
+    db_contents = data_frame.apply(tuple, axis=1).tolist()
+    return db_contents
+
+read_laptops_csv("server/laptop.csv")
