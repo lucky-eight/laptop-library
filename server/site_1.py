816d18066e05282d09404b5667747bbecbc2f5d0
Lulwa Alkhalifa <lulwa.m.alkhalifa@gmail.com>
2022-06-11 16:02:02 +0100

    simple database

--- server/site_1.py
+++ server/site_1.py
@@ -2,8 +2,11 @@
 
 # Imports
 from flask import Flask, render_template
+import sqlite3 as db
 
 app = Flask(__name__)
+conn = db.connect('my_database.db')
+c = conn.cursor()
 
 @app.route("/")
 def home():
@@ -17,4 +20,15 @@ def about():
     # return "About page goes here"
 
 if __name__ == "__main__":
+
+    c.execute("CREATE TABLE IF NOT EXISTS laptops (laptopid INTEGER PRIMARY KEY, name NVARCHAR(20), city NVARCHAR(20))")
+
+    # populate database
+    new_laptops = [(1, 'MacBook Pro 16 inch', 'London')]
+
+    c.executemany('INSERT INTO laptops VALUES (?, ?, ?)', new_laptops)
+
+    c.execute("SELECT * FROM laptops;")
+    print(c.fetchall())
     app.run(debug=True)
+
