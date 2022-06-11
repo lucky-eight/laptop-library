f869818979f547f66926b79aa5300ca691f9e779
Naomi <63055040+nkpe@users.noreply.github.com>
2022-06-11 19:09:59 +0100

    Full form and header file

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <div id="logo"></div>
+        <nav>
+            <a href="#"></a>
+        </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "ApplicationForm",
+
+  data() {
+    return {
+      user: {
+        name: "",
+        email: "",
+        address: {
+          street: "",
+          city: "",
+          postcode: "",
+        },
+      },
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
