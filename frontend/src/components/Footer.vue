f18a2359ba5dbadb4f288ebac67966956d94dedb
Naomi <63055040+nkpe@users.noreply.github.com>
2022-06-11 20:40:43 +0100

    Header and Footer components added

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer style="height: 50px; width: 100%; background-color: pink;">
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "GlobalFooter",
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
