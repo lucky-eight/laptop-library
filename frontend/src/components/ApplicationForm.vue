33105507f7b9eafd771f3bf855f89bba705ce50c
Naomi <63055040+nkpe@users.noreply.github.com>
2022-06-11 19:54:01 +0100

    Form updated with first and surname fields and 2 address lines

--- frontend/src/components/ApplicationForm.vue
+++ frontend/src/components/ApplicationForm.vue
@@ -1,14 +1,15 @@
 <template>
   <form id="user-form" action="#" method="POST">
-    <input id="name-field" type="text" name="name" placeholder="Full Name" required />
-    <input id="email-field" type="text" name="email" placeholder="Email" required />
-    <div id="address-line1">
-      <input id="house-number-field" type="text" name="house-number" placeholder="House Number" />
-      <input id="street-field" type="text" name="street-address" placeholder="Street" />
+    <div id="full-name-group">
+      <input id="first-name-field" type="text" name="first-name" placeholder="First Name" required />
+      <input id="surname-field" type="text" name="surname" placeholder="Surname" required />
     </div>
-    <input id="city-field" type="text" name="city" placeholder="City" />
-    <input id="postcode-code" type="text" name="postcode" placeholder="postcode" />
-    <button id="submit-form" type="submit" form="user-form"></button> 
+      <input id="email-field" type="text" name="email" placeholder="Email" required />
+      <input id="address-line1-field" type="text" name="address-line1" placeholder="Address Line 1" />
+      <input id="address-line2" type="text" name="address-line2" placeholder="Address Line 2" />
+      <input id="city-field" type="text" name="city" placeholder="City" />
+      <input id="postcode-code" type="text" name="postcode" placeholder="postcode" />
+      <button id="submit-form" type="submit" form="user-form"></button> 
   </form>
 </template>
 
@@ -22,9 +23,9 @@ export default {
         name: "",
         email: "",
         address: {
-          street: "",
-          city: "",
-          postcode: "",
+        street: "",
+        city: "",
+        postcode: "",
         },
       },
     };
