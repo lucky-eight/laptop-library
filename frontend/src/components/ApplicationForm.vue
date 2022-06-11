40dbf257f8084e8dbebf2da76b93b9c5bf2f5931
Naomi <63055040+nkpe@users.noreply.github.com>
2022-06-11 19:08:24 +0100

    Form added

--- frontend/src/components/ApplicationForm.vue
+++ frontend/src/components/ApplicationForm.vue
@@ -1,9 +1,31 @@
 <template>
-  <form>
-    <!-- Create form here -->
-  </form>
+  <header>
+  
+  </header>
+
+  <main>
+    <form id="user-form" action="#" method="POST">
+      <input id="name-field" type="text" name="name" placeholder="Full Name" required />
+      <input id="email-field type="text" name="email" placeholder="Email" required/>
+      <div id="address-line1">
+        <input id="house-number-field" type="text" name="house-number" placeholder="House Number" />
+        <input id="street-field" type="text" name="street-address" placeholder="Street" />
+      </div>
+      <input id="city-field" type="text" name="city" placeholder="City" />
+      <input id="postcode-code" type="text" name="postcode" placeholder="postcode" />
+      <button id="submit-form" type="submit" form="user-form"></button> 
+    </form>
+  </main>
+
+  <footer>
+  </footer>
+
+
 </template>
 
+
+
+<script src="https://kit.fontawesome.com/f469f6780c.js" crossorigin="anonymous"></script>
 <script>
 export default {
   name: "ApplicationForm",
