32028ea3542222747d569d6fe75034a255613999
technologeve <eve.nicole@icloud.com>
2022-06-12 04:14:21 +0100

    update result.vue

--- frontend/src/components/Result.vue
+++ frontend/src/components/Result.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="result-wrapper">
     <div v-if="isSuccess && deliveryDate" class="success-wrapper">
-      <h2>We've found a match!</h2>
-      <p>You can expect to receive a laptop by {{ deliveryDate }}</p>
+        <h2>We've found you a laptop!</h2>
+        <img src="../assets/match_success.png">
+        <p>APPLE MacBook Air 13.3" (2020) - M1, 256 GB SSD, Silver <br>
+        We'll be in touch soon with delivery or pick up instructions</p>
     </div>
     <div v-else class="unsuccess-wrapper">
-      <h2>Unfortunately there are no laptops currently available</h2>
-      <p>We will be in touch when we've found a match</p>
+        <h2>Unfortunately there are no laptops currently available</h2>
+        <img src="../assets/match_failure.png">
+        <p>You are on the waiting list; we will be in touch when we've found a match</p>
     </div>
   </div>
 </template>
@@ -28,14 +31,23 @@ export default {
 </script>
 
 <style scoped>
-  .success-wrapper{
-    text-align: center;
-  }
-  .unsuccess-wrapper{
-    text-align: center;
-  }
+    body{
+        text-align: center;
+        font-family: 'Jost', sans-serif;
+    }
+    .success-wrapper{
+        padding-top: 50px;
+    }
+    .unsuccess-wrapper{
+        padding-top: 50px;
+    }
 
-  h2{
-    padding: 15px;
-  }
+    h2{
+      padding: 15px;
+    }
+
+    img{
+        height: 100px;
+        width: 100px;
+    }
 </style>
